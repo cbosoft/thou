adb3990a816d31be775e4c977e896655e2f31102
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-11 15:53:10 +0100

    Testing the parser

--- src/crawler/main.cpp
+++ src/crawler/main.cpp
@@ -8,5 +8,8 @@ int main(int argc, const char **argv)
   (void) argv;
 
   Twirl &t = Twirl::get_singleton();
-  auto response = t.get("https://google.com");
+  //auto response = t.get("https://google.com");
+  auto response = t.get("https://en.wikipedia.org/wiki/List_of_most_popular_websites");
+  auto pt = response.to_plaintext();
+  std::cerr << pt << std::endl;
 }
