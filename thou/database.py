80826c5e6cf0b4f58bc24f3dbf80aed7ab252cb6
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-31 11:00:09 +0100

    Database class checks for .db file existance and correctness

--- thou/database.py
+++ thou/database.py
@@ -1,7 +1,34 @@
+import os
+import sqlite3 as sql
+
 class Database:
 
     def __init__(self, path):
         self.path = path
+        if os.path.exists(path):
+
+            if not os.path.isfile(path):
+                raise Exception("database exists and is not a file")
+
+            conn = sql.connect(path)
+            try:
+                res = conn.execute("SELECT * FROM A;");
+                print("table loaded")
+                conn.close()
+            except:
+                conn.close()
+                self.init_tables()
+        else:
+            self.init_tables()
+
+    def init_tables(self):
+        print("initialising database")
+        conn = sql.connect(self.path)
+        conn.execute("CREATE TABLE \"A\" (\"id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT);");
+        # TODO: create database scheme here
 
     def register_link(self, link):
         pass
+
+if __name__ == "__main__":
+    d = Database("a.db")
