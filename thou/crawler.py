02a85c1c5ec40c6bed3b907a95d7734f8b243ef4
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-19 21:45:04 +0100

    Extract metadata and link information from pages

--- thou/crawler.py
+++ thou/crawler.py
@@ -27,5 +27,30 @@ class Crawler:
     def get_links(self, url):
         '''get page pointed to by link, return link obj with meta data'''
         return []
+        # get page meta data
+        meta = self.get_meta(page)
+        self.database.register_link(url, meta)
+
+        # return links
+        links = self.get_links(page, url)
+        return links
+
+
+    def get_links(self, page, url):
+        '''given page, return list of links on page'''
+        links = list()
+        for tag in page.findAll(tags_with_href):
+            link = tag.get('href')
+            if not link.startswith('http'):
+                link = url+'/'+link
+            links.append(link)
+        return links
+
+
+    def get_meta(self, page):
+        s = page.getText()
+        s = remove_escapes(s)
+        s = top_words(s)
+        return s
 
 
