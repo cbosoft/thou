d9dd45d54138e1094a5796333f8c87739f00f0d6
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-20 17:08:14 +0100

    Improve results page style

--- thou/pages.py
+++ thou/pages.py
@@ -1,35 +1,52 @@
 class HTMLPage:
 
-    def __init__(self, **kwargs):
-        self.proto = '''
-        <HTML>
-          <HEAD>
-            <TITLE>{title}</TITLE>
-            <style>
-              * {{{{ font-family: Arial, Helvetica, sans-serif; }}}}
-            </style>
-          </HEAD>
-          <BODY>
-            <DIV style="display: flex; justify-content: center; align-items: center; height: 100%; flex-direction: column;">
-              {content}
-            </DIV>
-          <BODY>
-        </HTML>'''.format(**kwargs)
+    def __init__(self, proto):
+        self.proto = proto
 
     def as_bytes(self, **kwargs):
         s = self.proto.format(**kwargs)
         return s.encode()
 
 
-index_page = HTMLPage(title='thou', content='''
-            <span style="font-size: large; font-weight: bold; text-align: center;">thou</span>
-            <br>
-            <FORM action="/search" style="order: 1">
-              <input type="text" id="query" name="query">
-              <input type="submit" value="Search!">
-            </FORM>''')
+index_page = HTMLPage('''
+        <HTML>
+          <HEAD>
+            <TITLE>thou</TITLE>
+            <style>
+              * {{ font-family: Arial, Helvetica, sans-serif; }}
+            </style>
+          </HEAD>
+          <BODY>
+            <DIV style="display: flex; justify-content: center; align-items: center; height: 100%; flex-direction: column;">
+              <span style="font-size: large; font-weight: bold; text-align: center;">thou</span>
+              <br>
+              <FORM action="/search" style="order: 1">
+                <input type="text" id="query" name="query">
+                <input type="submit" value="Search!">
+              </FORM>
+            </DIV>
+          <BODY>
+        </HTML>''')
 
-results_page = HTMLPage(title='thou results', content='{results}')
+results_page = HTMLPage('''
+        <HTML>
+          <HEAD>
+            <TITLE>thou</TITLE>
+            <style>
+              * {{ font-family: Arial, Helvetica, sans-serif; }}
+            </style>
+          </HEAD>
+          <BODY>
+            <DIV style="margin: auto; width: 80%; min-width: 300px;">
+              <FORM action="/search" style="order: 1">
+                <a href="/home" style="color: black; text-decoration: none; font-weight: bold;">thou</a>
+                <input type="text" id="query" name="query">
+                <input type="submit" value="Search!">
+              </FORM><br>
+              {results}
+            </DIV>
+          <BODY>
+        </HTML>''')
 
 
 def format_result(idx, url, text, title, tags):
@@ -37,7 +54,7 @@ def format_result(idx, url, text, title, tags):
     #lines = lines[:min([10, len(lines)])]
     #text = '\n'.join(lines)
 
-    return (f'<div style="padding: 5px; order: {2*(idx+1)};">{idx+1}: <a href="{url}">{title}</a></div>'+
+    return (f'<div style="padding: 5px; order: {2*(idx+1)};">{idx+1}: <a href="{url}">{title}</a><span style="font-size: small; color: gray; padding: 5px;">{url}</span></div>'+
             f'<div style="order: {2*(idx+1) + 1}; color: gray;">{tags}</div>')
 
 def format_results(results, max=30):
