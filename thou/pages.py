ac0813953cf4575d6da925f4a2c17081e9f495a5
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-20 15:25:55 +0100

    Use common html centering for main page and results

--- thou/pages.py
+++ thou/pages.py
@@ -1,31 +1,34 @@
 class HTMLPage:
 
-    def __init__(self, proto):
-        self.proto = proto
+    def __init__(self, **kwargs):
+        self.proto = '''
+        <HTML>
+          <HEAD>
+            <TITLE>{title}</TITLE>
+            <style>
+              * {{{{ font-family: Arial, Helvetica, sans-serif; }}}}
+            </style>
+          </HEAD>
+          <BODY>
+            <DIV style="display: flex; justify-content: center; align-items: center; height: 100%; flex-direction: column;">
+              {content}
+            </DIV>
+          <BODY>
+        </HTML>'''.format(**kwargs)
 
     def as_bytes(self, **kwargs):
         s = self.proto.format(**kwargs)
         return s.encode()
 
-index = HTMLPage('''
-        <HTML>
-          <HEAD>
-            <TITLE>thou</TITLE>
-          </HEAD>
-          <BODY>
-            <FORM action="/search">
-              <b>Query?</b><input type="text" id="query" name="query">
-              <input type="submit" value="Search!">
-            </FORM>
-          <BODY>
-        </HTML>''')
 
-results = HTMLPage('''
-        <HTML>
-          <HEAD>
-            <TITLE>thou</TITLE>
-          </HEAD>
-          <BODY>
-            {results}
-          <BODY>
-        </HTML>''')
+index_page = HTMLPage(title='thou', content='''
+            <span style="font-size: large; font-weight: bold; text-align: center;">thou</span>
+            <br>
+            <FORM action="/search" style="order: 1">
+              <input type="text" id="query" name="query">
+              <input type="submit" value="Search!">
+            </FORM>''')
+
+results_page = HTMLPage(title='thou results', content='{results}')
+
+
