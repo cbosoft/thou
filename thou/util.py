59e17908375d8950e1e655f0c619915db3daf6b4
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-21 20:42:45 +0100

    Add func to escape quotes etc to make SQLite statement safe

--- thou/util.py
+++ thou/util.py
@@ -5,6 +5,10 @@ ANSI_ESCAPE_RE = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
 def remove_escapes(s):
     return ANSI_ESCAPE_RE.sub('', s)
 
+def sql_friendly(s):
+    s = s.replace('"', '""')
+    return s
+
 def first_n_chars(s, n):
     l = len(s)
     return s[:min([l, n])]
