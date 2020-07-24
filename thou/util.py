ace12ae9052d0c15453d2fa18519241957369f56
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-24 10:47:51 +0100

    Add Levenshtein distance calculator

--- thou/util.py
+++ thou/util.py
@@ -1,4 +1,5 @@
 import re
+from functools import lru_cache
 
 ANSI_ESCAPE_RE = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
 
@@ -12,3 +13,23 @@ def sql_friendly(s):
 def first_n_chars(s, n):
     l = len(s)
     return s[:min([l, n])]
+
+@lru_cache
+def levenshtein(s, t, i=None, j=None):
+
+    if i is None:
+        i = len(s) - 1
+
+    if j is None:
+        j = len(t) - 1
+
+    if min([i, j]) == 0:
+        return max([i + 1, j + 1])
+
+    ind = 0 if s[i] == t[j] else 1
+
+    return min([
+            levenshtein(s, t, i - 1, j) + 1,
+            levenshtein(s, t, i, j - 1) + 1,
+            levenshtein(s, t, i - 1, j - 1) + ind
+        ])
